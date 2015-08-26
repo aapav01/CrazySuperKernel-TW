8c4e5a59bdf2c3d33aea50102f8cbf112b174589
FrancescoCG <frank-97@hotmail.gr>
2015-08-26 20:41:01 +0300

    some changes

--- init/version.c
+++ init/version.c
@@ -39,10 +39,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (ShinySide)"
+	"Linux version " UTS_RELEASE " (CrazyGamer)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (ShinySide)"
+	" (CrazyGamer)"
 	" (" LINUX_COMPILER ") %s\n";
